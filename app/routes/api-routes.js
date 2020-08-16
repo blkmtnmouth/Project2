afc1bd7d10533f888496b9df4d96b5bd9970930f
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-08-16 19:11:52 -0400

    nothing changed...

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-//Dependency
-const Graph = require("../models/graphData.js");
-
-//Routes
-//Get request
-module.exports = function(app) {
-  app.get("/api/:graph?"), function(req, res) {
-    db.Post.findAll({})
-      .then(function(dbPost) {
-        res.json(dbPost);
-      })  
-    }
-//Post request
-  app.post("api/new", function(req, res) {
-      let graph = req.body;
-      let routeName = graph.name.replace(/\s+/g, "").toLowerCase();
-
-      Graph.create({
-          routename: routeName,
-          dataSet: graph.dataSet,
-          xAxis: graph.xAxis,
-          yAxis: graph.yAxis
-      });
-      res.status(204).end();
-  })  
-}
-=======
 //Dependency
 const Graph = require("../models/graphData.js");
 
@@ -62,4 +34,3 @@ module.exports = function(app) {
 
   });  
 }
->>>>>>> master
