1f230d82aa11288d9c8c2aae46a0f8be08031eb7
Tanisha <tanishabsandiford@gmail.com>
2020-08-20 17:04:56 -0400

    updated javascript

--- app/public/stylesheets/js/init.js
+++ app/public/stylesheets/js/init.js
@@ -2,6 +2,21 @@ const graphArea = $("#graphArea");
 const graphForm = $("#graphForm");
 const xAxisLabel = $("#xAxisLabel");
 const yAxisLabel = $("#yAxisLabel");
+const chartBtns = $(".chartBtn");
+
+
+const xvalEl = $("#xvalues")
+const addxbtn = $("#addxvalue")
+
+addxbtn.click(addXInputEl)
+
+function addXInputEl() {
+  var parent = $("<div>").addClass('xset')
+  var key = $("<input>").addClass('xkey')
+  var value = $("<input>").addClass('xvalue')
+  parent.append(key, value)
+  xvalEl.append(parent)
+}
 
 const keys = $("#xAxis")
 const values = $("#yAxis")
@@ -10,71 +25,46 @@ let type = "bar";
 $(document).on("ready", init)
 
 function init() {
-  $(".chartBtn").click(setType(), function(event) {
-    console.log(event.currentTarget.id);
-    type = event.currentTarget.id;
-  });
+   getGraph()
 };
 
-graphForm.on("click", function() {
-  event.preventDefault();
-  console.log(keys[0].value);
-});
+chartBtns.click(setType)
 
-function getGraph(e) {
-  if (e) {
-    e.preventDefault()
-  }
+graphForm.click(getGraph);
 
-  var labels = []
-  console.log(keys);
-  keys.each(function () {
-    const val = $(this).val()
-    labels.push(val)
-  })
+function getGraph() {
+  var labels = [], data = []
 
-  var datavals = []
-  values.each(function () {
-    const val = $(this).val()
-    datavals.push(val)
+  $(".xset").each(function(){
+    var label = $(this).find('.xkey').val()
+    var value = $(this).find('.xvalue').val()
+    labels.push(label)
+    data.push(value)
   })
 
   const baseQuery = "https://quickchart.io/chart?c="
-  const data = {
+  const CHART = {
     type,                                // Show a bar chart
     data: {
       labels,   // Set X-axis labels
+      //labels: [2012, 2013, 2014, 2015, 2016],   // Set X-axis labels
       datasets: [{
         label: 'Users',                         // Create the 'Users' dataset
-        data: datavals         // Add data to the chart
+        data          // Add data to the chart
+        //data: [120, 60, 50, 180, 120]           // Add data to the chart
       }]
-    },
-    options: {
-      scales: {
-        xAxis: [{
-          scaleLabel: {
-            display: true,
-            labelString: xAxisLabel.val()
-          }
-        }],
-        yAxis: [{
-          scaleLabel: {
-            display: true,
-            labelString: yAxisLabel.val()
-          }
-        }]
-      }
     }
   }
-  const chartString = JSON.stringify(data)
+  const chartString = JSON.stringify(CHART)
 
   graphArea.attr("src", baseQuery + chartString)
 }
 
 function setType() {
+  type = $(this).data('type')
   getGraph()
 
-  $(".select-type").each(function () {
+  chartBtns.each(function () {
     const _type = $(this).data("type")
     if (_type === type) {
       $(this).addClass('blue')
