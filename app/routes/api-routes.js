0fda9f1f4c61dd72003855c3103b7c12d5bfb4b7
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-09-05 18:08:48 -0400

    removed delete call and updated requests to send all data, specific data and get data

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,36 +1,53 @@
 //Dependency
-const Graph = require("../models/graphData.js");
+const db = require("../models");
+const router = require('express').Router();
 
-//Routes
-//Get request
-module.exports = function(app) {
-  app.get("/api/graph"), function(req, res) {
-    Graph.findAll({})
-      .then(function(res) {
-        res.json(res);
-      })  
-    };
-//Post request
-  app.post("api/graph", function(req, res) {
-      let graph = req.body;
-      let routeName = graph.name.replace(/\s+/g, "").toLowerCase();
-
-      Graph.create({
-          routename: routeName,
-          dataSet: graph.dataSet,
-          xAxis: graph.xAxis,
-          yAxis: graph.yAxis
-      });
-      res.status(204).end();
-  })
-  //Delete all request
-  app.delete("/api/:graph", function(req, res) {
-    // We just have to specify which todo we want to destroy with "where"
-    db.Graph.destroy({
-      truncate: true
-    }).then(function(dbGraph) {
-      res.json(dbGraph);
+// Get request to send all data from database
+router.get("/api/graph", (req, res) => {
+  db.Graph.findAll({})
+    .then(data => {
+      console.log(data);
+      res.json(data);
+    })
+    .catch(err => {
+      res.status(400).json(err);
     });
+});
 
-  });  
-}
+router.get("/api/graph/:name", (req, res) => {
+  db.Graph.findAll({
+    where: {
+      name: req.params.name
+    }
+  })
+  .then(data => {
+    console.log(data);
+    res.json(data);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
+});
+
+// Post request
+router.post("/api/graph/", ({ body }, res) => {
+  let bulkData = [];
+  for (i = 0; i < body.xVal.length; i++) {
+    bulkData.push({
+      name: body.name,
+      xVal: body.xVal[i],
+      yVal: body.yVal[i]
+    });
+  };
+
+  db.Graph.bulkCreate(bulkData)
+    .then(data => {
+      console.log(data);
+      res.json(data);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router
